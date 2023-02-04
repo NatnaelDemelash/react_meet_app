7d3bb8b96ab5ead5ef5888973eb3e0434c76038d
Natnael Demelash <natidemelash074@gmail.com>
2023-02-04 08:29:27 +0300

    setting up envionment for new project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import Todo from "./components/Todo";
 function App() {
-  return (
-    <div>
-      <h1>My Todo</h1>
-      <Todo title="Learn React basics" />
-      <Todo title="Master React " />
-      <Todo title="Explore full react pieces" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
